6660ea12f7b900b96344af8a715c802be935606a
Josh Wilkerson <jgwilkerson@gmail.com>
2025-03-17 15:10:25 -0400

    update QuickLinks widget UI

--- src/quick_links/widget.tsx
+++ src/quick_links/widget.tsx
@@ -1,14 +1,32 @@
 import React from "react"
-import { StackView, Link as TRLink, Text } from "@planningcenter/tapestry-react"
+import { StackView, Text } from "@planningcenter/tapestry-react"
 import { DisplayImage } from "../display_image"
 import type { Link } from "../quick_links"
 import { token } from "@planningcenter/tapestry"
 
 export const QuickLinksWidget = ({ content }: { content: Link[] }) => {
   return (
-    <StackView axis="vertical" spacing={2}>
+    <StackView axis="vertical" spacing={1} alignment="start">
       {content.map((link, index) => (
-        <StackView key={index} axis="horizontal" alignment="center" spacing={1}>
+        <StackView
+          key={index}
+          axis="horizontal"
+          alignment="center"
+          spacing={1}
+          as="a"
+          href={link.url}
+          paddingVertical={0.75}
+          paddingHorizontal={1}
+          radius={4}
+          target="_blank"
+          rel="noopener noreferrer"
+          css={{
+            textDecoration: "none",
+            "&:hover": {
+              backgroundColor: token("--t-fill-color-neutral-080"),
+            },
+          }}
+        >
           <StackView fontSize="20px">
             {link.icon.type === "emoji" ? (
               <Text inline>{link.icon.name}</Text>
@@ -20,14 +38,12 @@ export const QuickLinksWidget = ({ content }: { content: Link[] }) => {
               />
             )}
           </StackView>
-          <TRLink
-            to={link.url}
+          <Text
             color={token("--t-text-color-default-primary")}
             fontWeight={500}
-            external
           >
             {link.displayName}
-          </TRLink>
+          </Text>
         </StackView>
       ))}
     </StackView>
